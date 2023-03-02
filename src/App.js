5f9e5d9bd6d606f956ca4064ebd64e509e1a7be9
deramsey <ramseyd@clevelandcc.edu>
2023-03-02 16:55:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import ContactForm from './Contact.js';
+import {
+  BrowserRouter as Router, Routes, Route, Link
+} from "react-router-dom";
+
+const Homepage = () => {
+  return(
+    <h1>Welcome to our Website!</h1>
+  )
+}
+
+const About = () => {
+  return(
+    <h1>Here is information about us!</h1>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+    <header>
+      <Link to="/">HOME</Link>
+      <Link to="/about">ABOUT US</Link>
+      <Link to="/contact">CONTACT</Link>
+    </header>
+
+    <Routes>
+    <Route path="/" element= {<Homepage />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<ContactForm/>} />
+
+    </Routes>
+
+
+  </Router>
   );
 }
 
---
+++ src/Contact.js
@@ -0,0 +1,9 @@
+export default function ContactForm() {
+    return(
+        <div className="form">
+            <input type='text' className="inp" />
+            <textarea className="textbox"></textarea>
+            <button>Submit</button>
+        </div>
+    )
+}
